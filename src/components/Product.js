6ef25fa1091c5127c332bc535a78e3ea8ec7d294
Auezkhan Daniyar <nurovich14@gmail.com>
2020-12-03 12:01:59 +0600

    added modals (react-modals)

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
+import Modal from 'react-modal'
 
 export default class Product extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            product: null
+        }
+    }
+
+    openModal = (product) =>{
+        this.setState({product})
+    }
+
+    closeModal = () =>{
+        this.setState({product:null})
+    }
 
     render() {
+        const currentProduct = this.state.product
         return (
             <div>
                <ul className='products'>
                             {this.props.products.map(product => (
                                 <li key={product._id}>
                                     <div className='product'>
-                                        <a href={'#'+product._id}>
+                                        <a href={'#'+product._id} onClick={() => this.openModal(product)}>
                                             <img src={product.image} alt={product.title}></img>
                                             <p>{product.title}</p>
                                         </a>
@@ -20,6 +36,34 @@ export default class Product extends Component {
                                     </div>
                                 </li>))}
                         </ul>  
+                {currentProduct && 
+                <Modal isOpen={true} onRequestClose={this.closeModal}>
+                    <button onClick={this.closeModal} className='modal-close-btn'>x</button>
+                    <div className='modal-product-details'>
+                        <img src={currentProduct.image} alt={currentProduct.title}/>
+                        <div className='modal-product-details-description'>
+                            <p><strong>{currentProduct.title}</strong></p>
+                            <p>{currentProduct.description}</p>
+                            <p>Available Sizes: {" "}
+                                {currentProduct.availableSizes.map((size) =>(
+                                    <span>
+                                        {" "} 
+                                        <button className='button'>{size}</button>
+                                    </span>
+                                ))}
+                            </p>
+                            <div className='product-price'>
+                                <div>{currentProduct.price}</div>
+                                <button className='button primary' onClick={() =>{
+                                    this.props.addToCart(currentProduct)
+                                    this.closeModal()
+                                }}>Add to Cart</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </Modal>}
             </div>
         )
     }
